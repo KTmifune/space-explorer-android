48b4fbd8468989904b8fe5459fa8bc1a8a14f04b
Takuma Kato <k.t.mifune@gmail.com>
2025-04-09 00:27:23 +0900

    ディレクトリ作成/テーマカラー設定

---
+++ app/src/main/java/com/example/spaceexplorerapp/ui/designsystem/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.example.spaceexplorerapp.ui.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val primaryLight = Color(0xFFEA0017)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val secondaryLight = Color(0xFFB42621)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val tertiaryLight = Color(0xFF855000)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val backgroundLight = Color(0xFF625b71)
+val onBackgroundLight = Color(0xFFFFF8F7)
